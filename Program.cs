183d7354e0cdcf5d06edc1c1e1df76abe2de264a
陳仕軒 <chenshixuan@chenshixuandeMac-mini.local>
2025-10-21 20:53:02 +0800

    vercel v4

--- Program.cs
+++ Program.cs
@@ -18,31 +18,20 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: corsPolicy, policy =>
     {
         policy.WithOrigins(
-            "https://localhost:5173",                   
-            "https://performance-review-panel-frontend-o.vercel.app"           
+            "http://localhost:5173",
+            "https://performance-review-panel-frontend-o.vercel.app"
         )
         .AllowAnyHeader()
         .AllowAnyMethod();
     });
 });
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// 可在開發/生產皆啟用 Swagger（方便 demo）
+app.UseHttpsRedirection();
+app.UseCors(corsPolicy);
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// 啟動時自動套用 migration（會建立缺少的 table）
-//using (var scope = app.Services.CreateScope())
-//{
-    //var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    //db.Database.Migrate();
-//}
-
-app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
